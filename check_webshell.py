7600a8a53e93ba6d5226fbcd310e4a3ab66fb415
Gh057 <ghostprince.1648@gmail.com>
2019-09-17 16:25:42 +0700

    Update check_webshell.py
    Update API Key virustotal

--- check_webshell.py
+++ check_webshell.py
@@ -35,13 +35,14 @@ def request_url(link, domain):
     return str(code)+",NULL"
 
 def scan_vt(md5, i):
-    api = ['da0c17684c7b5b44b8896130ffa3ce3038f05d1906c6eac698d731eb685e37b2','e6c63afeeac23474b6f4a0440c25c4bcacf93e4057ca3713d22c8e03ecad460d','aea89b850b40ad7ac2e7cd289dd4e240744a75ff1ab5c2abb7c4af7cfe24003f','1bbc276423a9fd63c44f494a47f2d9bc87d85219982d44673d1141f27862ff5d','6c7f060be4da9621e2f04d1578d32377403ca5dba965dc07c310d10fdcbbe211','a4798dafcceec05ec3cb1750ed23b5fbe4b64ea4f0f2e74a7f9cd79984fd8991','635b34676a95af9705633e667b50fad144624ac24c2ce0bbfbcaea54f7abc7a6','d904a023c5b78283ac5703d76e3254a314d22ccacb9b07ba1a5a07e75ddb7f15','715d06bc845c66895bbe1d60ee52267eb6633ba004e78b74cc868b6f8835451f']
+    api = ['da0c17684c7b5b44b8896130ffa3ce3038f05d1906c6eac698d731eb685e37b2','e6c63afeeac23474b6f4a0440c25c4bcacf93e4057ca3713d22c8e03ecad460d','aea89b850b40ad7ac2e7cd289dd4e240744a75ff1ab5c2abb7c4af7cfe24003f','1bbc276423a9fd63c44f494a47f2d9bc87d85219982d44673d1141f27862ff5d','6c7f060be4da9621e2f04d1578d32377403ca5dba965dc07c310d10fdcbbe211','a4798dafcceec05ec3cb1750ed23b5fbe4b64ea4f0f2e74a7f9cd79984fd8991','635b34676a95af9705633e667b50fad144624ac24c2ce0bbfbcaea54f7abc7a6','d904a023c5b78283ac5703d76e3254a314d22ccacb9b07ba1a5a07e75ddb7f15','715d06bc845c66895bbe1d60ee52267eb6633ba004e78b74cc868b6f8835451f',
+    '6e92397035501237a8386cb268e5abe97c7f1f5381dc6161eabc461a72964857','f91f30096f9b73f502a47bcd5f0facf7263ec4457bc24cc45cc6de823eaa8b71','4d85000461cc03f6b06db4697238d7d60332751f86eac0c11263d650c2d6038b','097aee125146449efa78198764f1d70d2ffa7368b94fcf0c34c2fea8c5431e54','019a3a047d3bb117e8ae3cd6aea90481d8f7d73e3e4dd204e00b8f33aac9f3ea','6049c3bcfa4e4746de025065d16a48d07632ba247a6329e2a1952f73f782f8df','30cb21d628ecb6718d87f193c4f6b74ca402c453fe9e2ba8ce7830d01f16cdbe']
     url = 'https://www.virustotal.com/vtapi/v2/file/report'
     param = {'resource':md5,'apikey':api[i%len(api)],'allinfo': '0'}
     data = urllib.urlencode(param)
     result = urllib2.urlopen(url,data)
     jdata =  json.loads(result.read())
-    t = 60/4/len(api)
+    t = 60/4/len(api)+1
     time.sleep(t)
     if jdata['response_code']==0:
         return None
@@ -57,7 +58,7 @@ domain = sys.argv[2]
 s=s.split("\n")
 list_url=[]
 s[0]+=",Request,URL,Virustotal"
-i_md5 = s[0].split(",").index("md5")
+i_md5 = s[0].split(",").index("MD5")
 i=1
 for x in s[1:]:
     x = x.split(",")
